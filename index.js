d25a6d3b3b8effa1d7dddd666403770f67b0e831
limsehui <sehui9072@gmail.com>
2020-05-21 17:42:36 +0900

    study vanilla js

---
+++ index.js
@@ -0,0 +1,7 @@
+const title = document.getElementById("title");
+
+/* DOM = Document Object Module
+(https://developer.mozilla.org/ko/docs/Web/API/Document_Object_Model/%EC%86%8C%EA%B0%9C)*/
+title.innerHTML ="Hi im sehui !";
+title.style.color ="beige"
+document.title= "I own you now";
